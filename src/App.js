faf3deff55c5baf9c95f3e779f214b0d5d3dc843
ToluwalaseO <benjamoseni@gmail.com>
2024-11-26 11:11:05 -0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         </div>
         <div className="logo">Stellar</div>
         <nav className="nav">
-          <a href="#services">Services</a>
+          <a href="#services">Services <i class="fas fa-chevron-down"></i></a>
           <a href="#about">About us</a>
           <a href="#contact">Contact us</a>
           <button className="get-free-btn">Get it for free</button>
@@ -18,13 +19,17 @@ const App = () => {
       </header>
 
       <main className="main-content">
+        <div className="whole">
         <section className="hero">
           <h1>Transforming</h1>
           <h1>financial services with</h1>
-          <h1>innovations</h1>
+          <h1>innovation ∞</h1>
+
           <div className="powered">
             <h2>Powered by</h2>
-            <p>AI</p>
+            <div className="Aiptag">
+             AI
+            </div> 
           </div>
           <div className="stats">
             <div className="stat">
@@ -40,21 +45,30 @@ const App = () => {
             <button className="get-free-btn2">Get it for free</button>
           </div>
         </section>
+        </div>
 
         <section className="widgets">
           <div className="Aimage">
-            <img src="/rename.png" alt="AI Image" />
-          </div>
-          <div className="widget">
-            <h3>AI - Assistant</h3>
-            <div className="widget-icon">🔵</div>
+            <img src="/rename.jpg" alt="AI Image" />
           </div>
           <div className="bitcoin">
-            <h3>Ethereum</h3>
-            <p>$3,502.73</p>
-            <div className="chart">📈</div>
+          <div className="imageer">
+          <img src="/image.jpg" alt="bitcoin" />
+        </div>
           </div>
         </section>
+        <div className="widget">
+            <h3>AI - Assistant</h3>
+            <h4>🔵</h4>
+            <section className="features">
+          <div className="feature-card1">
+            <h3><i className="fa fa-university" style={{ fontSize: "30px" }}></i>Digital Banking</h3>
+          </div>
+          <div className="feature-card2">
+            <h3><i className="fa fa-link" style={{ fontSize: "30px" }}></i>Seamless Integration</h3>
+          </div>
+        </section>
+          </div>
       </main>
     </div>
   );
